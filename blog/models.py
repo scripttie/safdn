f44fd146c5d0e186809a325bcfa443d47897bfd8
Kago <eddiekago@gmail.com>
2017-10-18 18:56:42 +0300

    modified blog with more models

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
-from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.models import Page, Orderable
 from wagtail.wagtailcore.fields import RichTextField
-from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 from wagtail.wagtailsearch import index
+
 import time
+
+from modelcluster.fields import ParentalKey
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
 
 # Create your models here.
 class BlogIndexPage(Page):
@@ -16,6 +22,46 @@ class BlogIndexPage(Page):
 		context['blogpages'] = blogpages
 		return context
 
+class BlogPageTag(TaggedItemBase):
+	content_object = ParentalKey('BlogPage', related_name='tagged_items')
+
+class BlogPage(Page):
+	date = models.DateField("Post date")
+	intro = models.CharField(max_length=250)
+	body = RichTextField(blank=True)
+
+	def main_image(self):
+		gallery_item = self.gallery_images.first()
+		if gallery_item:
+			return gallery_item.image
+		else:
+			return None
+
+	search_fields = Page.search_fields + [
+	index.SearchField('intro'),
+	index.SearchField('body'),
+	]
+
+	content_panels = Page.content_panels + [
+	MultiFieldPanel([
+		FieldPanel('date'),
+		# FieldPanel('tags'),
+		], heading="Blog information"),
+	FieldPanel('date'),
+	FieldPanel('intro'),
+	FieldPanel('body', classname="full"),
+	InlinePanel('gallery_images', label="Gallery images"),
+	]
+
+class BlogPageGalleryImage(Orderable):
+	page = ParentalKey(BlogPage, related_name='gallery_images')
+	image = models.ForeignKey('wagtailimages.Image', on_delete=models.CASCADE, related_name='+')
+	caption = models.CharField(blank=True, max_length=250)
+
+	panels = [
+	ImageChooserPanel('image'),
+	FieldPanel('caption'),
+	]
 	# date = models.DateField("Post date")
 	# intro = models.CharField(max_length=250)
 	# body = RichTextField(blank=True)
